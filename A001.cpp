83e7a826b494d79e2457ba3c57160cf8ae412527
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-07-02 16:23:22 +0900

    Feat: complete A001.cpp

--- A001.cpp
+++ A001.cpp
@@ -5,24 +5,67 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <cstdlib>
 
 using namespace std;
 
 int main(){
-    string s;
+    string input_g;
     string sub1;
+    string input_s;
+    string sub2;
 
-    getline(cin, s);
-    sub1 = s.substr(1, s.size()-2);
+    getline(cin, input_g);
+    getline(cin, input_s);
+    sub1 = input_g.substr(1, input_g.size() - 2);
+    sub2 = input_s.substr(1, input_s.size() - 2);
 
-    stringstream ss(sub1);
+    stringstream ss1(sub1);
+    stringstream ss2(sub2);
+
     string tok;
+    int g_count = 0;
+    int s_count = 0;
 
-    while(getline(ss, tok, ',')){
-        cout << tok << endl;
-        cout << typeid(tok).name() << endl;
+    while(getline(ss1, tok, ',')){
+        g_count++;
     }
 
+    while(getline(ss2, tok, ',')){
+        s_count++;
+    }
+
+    int* g = (int*)malloc(sizeof(int) * g_count);
+    int* s = (int*)malloc(sizeof(int) * s_count);
+
+    ss1.clear();
+    ss2.clear();
+    ss1.str(sub1);
+    ss2.str(sub2);
+
+    int index=0;
+    while(getline(ss1, tok, ',')){
+        g[index++] = stoi(tok);
+    }
+
+    index=0;
+    while(getline(ss2, tok, ',')){
+        s[index++] = stoi(tok);
+    }
+
+    int count=0;
+
+    if(g_count < s_count){
+        for(int i=0; i<g_count; i++){
+            if(g[i] <= s[i]) count++;
+        }
+    } else{
+        for(int i=0; i<s_count; i++){
+            if(g[i] <= s[i]) count++;
+        }
+    }
+
+    cout << count << endl;
     
     return 0;
 }
