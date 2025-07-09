0fc5bfe26d8757de83a8128862e299e4d178249a
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-07-10 00:43:57 +0900

    Feat: 일단 중간

--- A028.cpp
+++ A028.cpp
@@ -6,12 +6,31 @@ using namespace std;
 int main(){
     string A;
     string B;
+    string result;
 
     cin >> A >> B;
 
     int time;
 
+    if(A.size() < B.size()){
+        time = A.size();
+    } else time = B.size();
 
+    int carry;
+    int ret;
+
+    for(int i=time-1; i>=0; i--){
+        ret = ( A[i] - '0' ) + ( B[i] - '0' );
+        ret += carry;
+        result = result + to_string(ret % 10);
+        carry = ret / 10;
+    }
+
+    for(int i=time-1; i>=0; i--){
+        cout << result[i];
+    }
+
+    cout << endl;
     
     return 0;
 }
